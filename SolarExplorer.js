dad599fa48cf74072d6f46cc1cc3665de3206b2b
정하리 <50551231+jha2ee@users.noreply.github.com>
2022-10-23 16:24:40 +0900

    Blackhole location
    blackhole position을 spaceship 의 현재 위치 기준에서 가장 가까운 좌표로 이동,
    blackhole interaction handler js파일 분리 예정

--- SolarExplorer.js
+++ SolarExplorer.js
@@ -365,16 +365,20 @@ window.onload = function init()
 	/* create black hole */
 	// change : create to 4 ways
 	const loader = new THREE.GLTFLoader();
+	
 	loader.load('./images/blackhole/scene.gltf', function(gltf){
-	  blackhole = gltf.scene.children[0];
-	  blackhole.scale.set(10, 10, 10);
-	  blackhole.position.set(400, 0, 0);
-	  scene.add(gltf.scene);
-	  animate();
-	  
-	}, undefined, function (error) {
-		console.error(error);
-	});
+		blackhole = gltf.scene.children[0];
+		blackhole.scale.set(10, 10, 10);
+		
+		blackhole.position.set(400, 0, 0);		
+		scene.add(gltf.scene);
+		animate(blackhole);
+		  
+		}, undefined, function (error) {
+			console.error(error);
+		});
+
+	
 	// for rotating black hole 
 	function animate(time) {
 
@@ -383,10 +387,24 @@ window.onload = function init()
       	const speed = 1 * .3;
       	const rot = time * speed;
       	blackhole.rotation.x = rot;
-  
+		// spaceship의 위치에 따라 blackhole 방향 설정
+		if (spaceship.Position.x < 100) {
+			if (spaceship.Position.y < 100) {
+				blackhole.position.set(-200, -200, 0);
+			}
+			else blackhole.position.set(-200, 200, 0);
+		}
+		else {
+			if (spaceship.Position.y < 100) {
+				blackhole.position.set(200, -200, 0);
+			}
+			else blackhole.position.set(200, 200, 0);
+		}
+		
 	   renderer.render(scene,camera);
 	   requestAnimationFrame(animate);
 	}
+
 
 	var spaceship = new SpaceshipController();
 	var previousTime = null;
